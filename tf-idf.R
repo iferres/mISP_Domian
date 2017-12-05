268ae68e33b3938aa103fe45d426a9cb538c2608
iferres <igferres@gmail.com>
2017-12-05 09:37:26 -0300

    created tf idf

---
+++ tf-idf.R
@@ -0,0 +1,28 @@
+dom_tfidf <- function(df){
+  
+  nr <- nrow(df)
+  
+  tf <- apply(df, 2 , function(x){
+    x/sum(x)
+  })
+  
+  idf <- apply(df, 2, function(x){
+    
+    p <- sum(ifelse(x>0, 1L, 0L))
+    
+    log(nr/p)
+  })
+  
+  o <- vapply(1:ncol(df), function(x){
+    
+    tf[, x] * idf[x]
+    
+  }, FUN.VALUE = rep(1, nrow(df)))
+  
+  colnames(o) <- colnames(df)
+  
+  return(o)
+}
+
+
+
